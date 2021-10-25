8fe454b74f0300c46fd99c2b7ecec642157792c9
Amine GABSI <mamine.gabsi@gmail.com>
2021-10-25 14:36:52 +0100

    week2

--- WarmUp2.js
+++ WarmUp2.js
@@ -5,6 +5,16 @@
 // counting(5); // => '1, 2, 3, 4, 5'
 // counting(1); // => '1'
 // counting(3); // => '1, 2, 3'
+function counting(n) {
+  var i = 1;
+  var result = [];
+  while (i < n + 1) {
+    result.push(i);
+    i++;
+  }
+  result.toString();
+  return result;
+}
 
 // 2 -  Meet & Greet: n numbers are coming to a party for numbers. Introduce them each to all the guests as they arrive.
 //You should end up with output looking something like this one for n = 5
@@ -13,7 +23,36 @@
 //  welcome 3, meet 1 and 2
 //  welcome 4, meet 1, 2 and 3
 //  welcome 5, meet 1, 2, 3 and 4
-
+function meetAndGreet(n) {
+  var counter = 1;
+  var innerCount;
+  var total = "";
+  var linebreak = "/n";
+  var comma = ", ";
+  while (counter <= n) {
+    if (n === 1) {
+      total = "welcome " + counter;
+    } else if (counter === 1) {
+      total = "welcome " + counter + linebreak;
+    } else {
+      total = total + "welcome " + counter + comma + "meet ";
+      innerCount = 1;
+      while (innerCount < counter) {
+        if (innerCount === counter - 2) {
+          total = total + innerCount + " and ";
+        } else if (innerCount === counter - 1) {
+          total = total + innerCount;
+        } else {
+          total = total + innerCount + comma;
+        }
+        innerCount++;
+      }
+      total = total + linebreak;
+    }
+    counter++;
+  }
+  return total;
+}
 // USE RECURSION
 // 3- Repeating a String n Times: Let's write a function called repeatString that takes two parameters: a string str,
 //which is the string to be repeated, and count -- a number representing how many times the string str should be repeated
@@ -21,3 +60,9 @@
 // repeatString('dog', 1); // => 'dog'
 // repeatString('dog', 2); // => 'dog' + 'dog' => 'dogdog'
 // repeatString('dog', 3); // => 'dog' + 'dog' + 'dog' => 'dogdogdog'
+function repeatString(word, nbr) {
+  if (nbr === 0) {
+    return "";
+  }
+  return word + repeatString(word, nbr - 1);
+}
--- WarmUp3.js
+++ WarmUp3.js
@@ -5,7 +5,12 @@
 // repeatString('dog', 1); // => 'dog'
 // repeatString('dog', 2); // => 'dog' + 'dog' => 'dogdog'
 // repeatString('dog', 3); // => 'dog' + 'dog' + 'dog' => 'dogdogdog'
-
+function repeatString(word, nbr) {
+  if (nbr === 0) {
+    return "";
+  }
+  return word + repeatString(word, nbr - 1);
+}
 // // USE RECURSION
 
 //2
@@ -13,12 +18,29 @@
 // -  iterate from x to y.
 // -  return array contain the even values,
 //  ex:  isEven(1,10) => [2,4,6,8,10]
-
+function isEven(x, y) {
+  var result = [];
+  for (let i = x; i < y + 1; i++) {
+    if (i % 2 === 0) {
+      result.push(i);
+    } else {
+      continue;
+    }
+  }
+  return result;
+}
 //3
 // write a function named sum that
 // - Use a while loop to add up the numbers from x to y.
 // ex sum(1,5) => 15
-
+function sum(x, y) {
+  var result = 0;
+  while (x < y + 1) {
+    result += x;
+    x++;
+  }
+  return result;
+}
 //4
 // Write a function named factorial that
 // -  Use Recursion to calculate the factorial of a number
@@ -26,7 +48,12 @@
 //than or equal to n
 // - 5! = 5*4*3*2*1 = 120
 // ex : factorial(5) => 120
-
+function factorial(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
 //5
 // write a function named decimals
 //- the function will format a number up to specified decimal places
@@ -39,12 +66,29 @@
 //      decimals(2.100212, 3) ==> '2.100'
 //      decimals(2100, 2) ==> '2100.00'
 //
-
+function decimals(x, y){
+  if(typeof x != typeof y && typeof x != "number"){
+    return false
+  } return x.toFixed(y).toString()
+}
 //6
 // write a function to Reverse an array without use array.reverse() method:
 // ex: reverseArray([1,2,3]) ==> [3,2,1]
-
+function reverseArray(arr) {
+  var result = [];
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    result.unshift(element);
+  }
+  return result;
+}
 //7
 // Write a function called laugh() that takes one parameter,
 // num that represents the number of "ha"s to return.
 // laugh(6); => "hahahahahaha"
+function laugh(n) {
+  if (n === 0) {
+    return "";
+  }
+  return "ha" + laugh(n - 1);
+}
