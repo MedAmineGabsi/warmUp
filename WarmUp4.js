3c538d4767a3b4eed96d9e5bc6609be4c1b5926f
Amine GABSI <mamine.gabsi@gmail.com>
2021-10-28 17:17:11 +0100

    warmUp 4

---
+++ WarmUp4.js
@@ -0,0 +1,32 @@
+// 1 - Write a function named objKey that take an object as a parameter and returns an array containing the keys of the object
+// objKey({name:'ahmed',age:20}) ==> ['name','age']
+
+function objKey(obj) {
+  var result = [];
+  for (var key in obj) {
+    result.push(key);
+  }
+  return result;
+}
+
+// 2 - Write a function named objLength to get the length of a JavaScript object
+// objLength({name:'ahmed',age:20}) ==> 2
+
+function objLength(obj) {
+  var result = 0;
+  for (var key in obj) {
+    result += 1;
+  }
+  return result;
+}
+
+// 3 - Write a function named objSort to sort an array of JavaScript objects based on the id
+// objSort([{id:2,value:50},{id:0,value:70},{id:1,value:40}]) ==> [{id:0,value:70},{id:1,value:40},{id:2,value:50}]
+
+function objSort(array) {
+  var result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.splice(array[i].id, 0, array[i]);
+  }
+  return result;
+}
