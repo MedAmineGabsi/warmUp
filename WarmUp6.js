11fe34b6af1dc5e087f90f763fbab181014d661a
Amine GABSI <mamine.gabsi@gmail.com>
2021-11-27 10:29:53 +0100

    warmUp 6

--- WarmUp6.js
+++ WarmUp6.js
@@ -2,6 +2,27 @@
 // return array of prime numbers in the object
 //using each.
 
+function isPrimeArray(obj) {
+  var values = Object.values(obj);
+  return filter(values, function (n) {
+    var hint = function (n) {
+      if (n === 1) {
+        return true;
+      } else if (n === 2) {
+        return true;
+      } else {
+        for (var x = 2; x < n; x++) {
+          if (n % x === 0) {
+            return false;
+          }
+        }
+        return true;
+      }
+    };
+    return hint(n) === true;
+  });
+}
+
 // 2 Write a function called agesToMinutes that,
 // accepts an array of object as parameter and
 // return an array of objects after converting the ages from years to minutes
@@ -39,6 +60,11 @@
 //        age: 33
 //  }];
 //  agesToMinutes(people);
+function agesToMinutes(people) {
+  return map(people, function (obj) {
+    return obj.age * 365 * 24 * 60;
+  });
+}
 // ~~~~~~~~~~~~~~~~ already implemented functions ~~~~~~~~~~~~~~~~
 function each(coll, func) {
   if (Array.isArray(coll)) {
@@ -59,6 +85,23 @@ function map(coll, f) {
   }
   each(coll, function (element, key) {
     acc[key] = f(element, key);
+  });
+  return acc;
+}
+
+function filter(coll, predicate) {
+  var acc = [];
+  if (!Array.isArray(coll)) {
+    acc = {};
+  }
+  each(coll, function (value, key) {
+    if (predicate(value, key)) {
+      if (Array.isArray(coll)) {
+        acc.push(value);
+      } else {
+        acc[key] = value;
+      }
+    }
   });
   return acc;
 }
